37212474340b800e02aa2ae562c0ad78b7bc5030
Thomas Perron <thomas.perron@ticketmaster.com>
2022-03-01 18:04:53 -0500

    Completed 'Resolvers' pathway

---
+++ odyssey-lift-off-part2/server/src/resolvers.js
@@ -0,0 +1,17 @@
+// src/resolvers.js
+
+const resolvers = {
+  Query: {
+    // get all tracks, will be used to populate the homepage grid of our web client
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+  },
+};
+
+module.exports = resolvers;
